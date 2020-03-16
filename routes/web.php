761f47d19e846b5c4fe5f96f2d4320e58659f6a6
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-16 10:13:13 -0300

    ⤳ Cria rota de download

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PostControlador@index');
 Route::post('/', 'PostControlador@store');
 Route::delete('/{id}', 'PostControlador@destroy');
+Route::get('/download/{id}', 'PostControlador@download');
